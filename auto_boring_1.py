daf39799932dd3a45cac56eeddefa0d4c8acbf98
FitzFrosty <47310268+FitzFrosty@users.noreply.github.com>
2019-02-05 16:35:10 -0600

    Add files via upload

---
+++ auto_boring_1.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("Please, tell me your name: ")
+user_name = input()
+print("Nice to meet you" + user_name)
+print("Did you know the length of your name is: ")
+print(len(user_name))
+print("How old are you?")
+user_age = input()
+print("Wow! You will be " + str(int(user_age) +1) + " in a year")
+
